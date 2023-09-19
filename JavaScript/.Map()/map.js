26a057f7cf3b0e32e5e0bf685355353c9075f15d
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-09-19 21:13:05 +0100

    Updates

---
+++ JavaScript/.Map()/map.js
@@ -0,0 +1,18 @@
+const array1 = [1, 4, 9, 16];
+const elements = ["Fire", "Air", "Water"];
+const collection = [
+  "Korean Petite Bussy",
+  "Chinese Petite Bussy",
+  "Thai Bussy",
+  "Nordic Petite Bussy",
+  "English Petite Bussy",
+  "English Short Petite Mature",
+  "English Chubby Short Mature",
+  "English Petite Short Granny",
+  "English Chubby Short Granny",
+  "Hindu Bramin Bussy",
+  "Hindu Bramin Chubby Short Granny",
+  "Hindu Bramin Petite Short Granny",
+  "Hindu Bramin Petite Mature",
+  "Hindu Bramin Short Mature",
+];
