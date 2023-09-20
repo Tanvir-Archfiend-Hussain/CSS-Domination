1ceb8c1a6fa5a888f7e13de95ff2f9ec1a180ada
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-09-20 15:47:20 +0100

    Update

--- To Do LIst/js/main.js
+++ To Do LIst/js/main.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+window.addEventListener("load", () => {
+  const form = document.querySelector("#new-task-form");
+  const input = document.querySelector("#new-task-input");
+  const list_el = document.querySelector("#task");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+  });
+});
