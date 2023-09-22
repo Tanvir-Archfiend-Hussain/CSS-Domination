44a409ce27d7ff956942cdfe968472bfd8c9087a
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-09-22 23:28:21 +0100

    console.log()

--- JS/Multiply/Multiply.js
+++ JS/Multiply/Multiply.js
@@ -3,6 +3,6 @@ function Multiply(num1, num2) {
   return result;
 }
 
-Multiply(4, 7);
-Multiply(20, 20);
-Multiply(0.5, 3);
+console.log(Multiply(4, 7));
+console.log(Multiply(20, 20));
+console.log(Multiply(0.5, 3));
