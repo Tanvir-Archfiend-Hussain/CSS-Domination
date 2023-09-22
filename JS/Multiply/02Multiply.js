f2115120d0995bc736ef87bb608dfbf70639e90f
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-09-23 00:09:48 +0100

    Multiplay

---
+++ JS/Multiply/02Multiply.js
@@ -0,0 +1,8 @@
+function Multiply(num1, num2) {
+  let result = num1 + " " + num2;
+  return result;
+}
+
+console.log(Multiply("Dumb", "ASS"));
+console.log(Multiply(20, 20));
+console.log(Multiply(0.5, 3));
